a7c51df46a2ddeda4c64558bf72d00bcef114ed8
enddrop28 <mea.enddrop@gmail.com>
2021-08-22 21:57:24 +0000

    [Start]3-9

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
 
   end
 
@@ -14,13 +16,9 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blog_path(@blog.id)
-    else
-      render :new
-    end
-
+    blog = Blog.new(blog_params)
+    binding.pry    blog.save
+    redirect_to blog_path(blog.id)
   end
 
   def edit
